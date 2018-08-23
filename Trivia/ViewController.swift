790b8cbb547bf027b26c86887d8191c970791003
Jordan <jordan.jones@detroitlabs.com>
2018-08-23 15:58:01 -0400

    Created question dictionary

--- Trivia/ViewController.swift
+++ Trivia/ViewController.swift
@@ -9,6 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var questions = ["question 1" : "a",
+                     "question 2" : "a",
+                     "question 3" : "a",
+                     "question 4": "a",
+                     "question 5": "a",
+                     "question 6": "a",
+                     "question 7": "a",
+                     "question 8": "a",
+                     ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
