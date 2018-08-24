63e659715c06ad989c03967ec92741a77bbc7571
Jordan <jordan.jones@detroitlabs.com>
2018-08-23 21:29:58 -0400

    Deleted array of tuples and switched to a question struct

--- Trivia/ViewController.swift
+++ Trivia/ViewController.swift
@@ -8,38 +8,60 @@
 
 import UIKit
 
+struct Question {
+    
+    var question: String
+    var correctAnswer: String
+    var wrongAnswer: String
+    
+}
+
 class ViewController: UIViewController {
     @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var buttonOneLabel: UIButton!
+    @IBOutlet weak var buttonTwoLabel: UIButton!
+    @IBOutlet weak var rightOrWrongLabel: UILabel!
     
-    var questions: [(String, String)] = [("Question 1", "a"),
-                                         ("Question 2", "a"),
-                                         ("Question 3", "a"),
-                                         ("Question 4", "a"),
-                                         ("Question 5", "a"),
-                                         ("Question 6", "a"),
-                                         ("Question 7", "a"),
-                                         ("Question 8", "a")
-                                        ]
-
+    
+    //generate questions with my struct
+    let questionOne = Question(question: "Question 1", correctAnswer: "a", wrongAnswer: "b")
+    let questionTwo = Question(question: "Question 2", correctAnswer: "a", wrongAnswer: "b")
+    let questionThree = Question(question: "Question 3", correctAnswer: "a", wrongAnswer: "b")
+    let questionFour = Question(question: "Question 4", correctAnswer: "a", wrongAnswer: "b")
+    
+    lazy var questionBank = [questionOne, questionTwo, questionThree, questionFour]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        questionLabel.text = questions[0].0
+        generatateQuestionLabel()
+        setButtonLabels()
+    }
+    
+    
+    @IBAction func buttonOne(_ sender: UIButton) {
+        buttonOneLabel.setTitle(questionBank[0].correctAnswer, for: .normal)
+    }
+    
+
+    @IBAction func buttonTwo(_ sender: UIButton) {
+        buttonTwoLabel.setTitle(questionOne.wrongAnswer, for: .normal)
+        print(questionBank[0].wrongAnswer)
+    }
+    
+    func generatateQuestionLabel() {
+        questionLabel.text = questionBank[0].question
+    }
+    
+    func checkAnswer() {
+        
+        
         
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func setButtonLabels() {
+        buttonOneLabel.setTitle(questionOne.correctAnswer, for: .normal)
+        buttonTwoLabel.setTitle(questionOne.wrongAnswer, for: .normal)
     }
-    
-    
-    @IBAction func answerA(_ sender: UIButton) {
-    }
-    
-    @IBAction func answerB(_ sender: UIButton) {
-    }
-    
 }
-
