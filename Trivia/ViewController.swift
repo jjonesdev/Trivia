c1b28747ca78fce24ab74b6fac67fcc2e24958a1
Jordan <jordan.jones@detroitlabs.com>
2018-08-26 21:25:45 -0400

    Added four answers and two buttons. Linked all tapped button behavior to one function. Created a buttons array to loop through and set button titles with. Now have a total of 4 answers in Question struct. Redesigned the storyboard.

--- Trivia/ViewController.swift
+++ Trivia/ViewController.swift
@@ -7,61 +7,102 @@
 //
 
 import UIKit
+import GameKit
 
 struct Question {
     
     var question: String
     var correctAnswer: String
-    var wrongAnswer: String
+    var wrongAnswerOne: String
+    var wrongAnswerTwo: String
+    var wrongAnswerThree: String
     
 }
 
 class ViewController: UIViewController {
+    
     @IBOutlet weak var questionLabel: UILabel!
-    @IBOutlet weak var buttonOneLabel: UIButton!
-    @IBOutlet weak var buttonTwoLabel: UIButton!
+    @IBOutlet weak var buttonOne: UIButton!
+    @IBOutlet weak var buttonTwo: UIButton!
+    @IBOutlet weak var buttonThree: UIButton!
+    @IBOutlet weak var buttonFour: UIButton!
     @IBOutlet weak var rightOrWrongLabel: UILabel!
+
+    var answerButtons: [UIButton] = []
+    var questions: [Question]?
+    var rightOrWrong: [String] = []
+    var rightOrWrongIndex = 0
     
-    
-    //generate questions with my struct
-    let questionOne = Question(question: "Question 1", correctAnswer: "a", wrongAnswer: "b")
-    let questionTwo = Question(question: "Question 2", correctAnswer: "a", wrongAnswer: "b")
-    let questionThree = Question(question: "Question 3", correctAnswer: "a", wrongAnswer: "b")
-    let questionFour = Question(question: "Question 4", correctAnswer: "a", wrongAnswer: "b")
-    
-    lazy var questionBank = [questionOne, questionTwo, questionThree, questionFour]
+    var index = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        questions = [
+                    Question(question: "Question 1", correctAnswer: "a", wrongAnswerOne: "b", wrongAnswerTwo: "c", wrongAnswerThree: "d"),
+                    Question(question: "Question 2", correctAnswer: "a", wrongAnswerOne: "b", wrongAnswerTwo: "c", wrongAnswerThree: "d"),
+                    Question(question: "Question 3", correctAnswer: "a", wrongAnswerOne: "b", wrongAnswerTwo: "c", wrongAnswerThree: "d"),
+                    Question(question: "Question 4", correctAnswer: "a", wrongAnswerOne: "b", wrongAnswerTwo: "c", wrongAnswerThree: "d")
+                    ]
+        
+        index = randomQuestionIndex()
+        
+        answerButtons = [buttonOne, buttonTwo, buttonThree, buttonFour]
+        
+        rightOrWrong = [questions![index].correctAnswer, questions![index].wrongAnswerOne, questions![index].wrongAnswerTwo, questions![index].wrongAnswerThree]
         
         generatateQuestionLabel()
         setButtonLabels()
     }
     
     
-    @IBAction func buttonOne(_ sender: UIButton) {
-        buttonOneLabel.setTitle(questionBank[0].correctAnswer, for: .normal)
-    }
+//    @IBAction func buttonOne(_ sender: UIButton) {
+//        buttonOneLabel.setTitle(rightOrWrong[0], for: .normal)
+//        if buttonOneLabel.currentTitle == questions![index].correctAnswer {
+//            rightOrWrongLabel.text = "Correct"
+//            view.backgroundColor = UIColor.cyan
+//        } else {
+//            rightOrWrongLabel.text = "Wrong"
+//        }
+//    }
+//
+//
+//    @IBAction func buttonTwo(_ sender: UIButton) {
+//        buttonTwoLabel.setTitle(rightOrWrong[1], for: .normal)
+//        if buttonTwoLabel.currentTitle == questions![index].correctAnswer {
+//            rightOrWrongLabel.text = "Correct"
+//            view.backgroundColor = UIColor.cyan
+//        } else {
+//            rightOrWrongLabel.text = "Wrong"
+//        }
+//    }
     
-
-    @IBAction func buttonTwo(_ sender: UIButton) {
-        buttonTwoLabel.setTitle(questionOne.wrongAnswer, for: .normal)
-        print(questionBank[0].wrongAnswer)
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        generatateQuestionLabel()
+        setButtonLabels()
+        
     }
     
     func generatateQuestionLabel() {
-        questionLabel.text = questionBank[0].question
+        index = randomQuestionIndex()
+        questionLabel.text = questions![index].question
     }
     
-    func checkAnswer() {
-        
-        
-        
+    func randomQuestionIndex() -> Int {
+        let questionIndex = Int(arc4random_uniform(UInt32(questions!.count)))
+        return questionIndex
     }
+    
     
     func setButtonLabels() {
-        buttonOneLabel.setTitle(questionOne.correctAnswer, for: .normal)
-        buttonTwoLabel.setTitle(questionOne.wrongAnswer, for: .normal)
+        rightOrWrong = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: rightOrWrong) as! [String]
+        rightOrWrongIndex = 0
+        for button in answerButtons {
+            print(button)
+            button.setTitle(rightOrWrong[rightOrWrongIndex], for: .normal)
+            rightOrWrongIndex += 1
+        }
     }
 }
